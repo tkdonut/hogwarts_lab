df72053278d056afe126973b3506c30e85268227
Gerry Waterston <gwaterston@hotmail.co.uk>
2018-12-12 16:58:00 +0000

    Update complete

--- students_controller.rb
+++ students_controller.rb
@@ -3,11 +3,17 @@ require('sinatra/contrib/all')
 require_relative('models/student')
 require_relative('models/house')
 also_reload('./models/*')
+require('pry')
 
 # index
 get '/' do
   @students = Student.all()
   erb(:index)
+end
+# new
+get '/new' do
+  # @houses = House.all
+  erb(:new)
 end
 # show
 get '/:id' do
@@ -15,12 +21,24 @@ get '/:id' do
   erb(:show)
 end
 
-# new
-
 # create
+post '/' do
+  @student = Student.new(params)
+  @student.save
+  redirect to '/'
+end
 
 # edit
-
+get '/edit/:id' do
+  @student = Student.find(params[:id])
+  erb(:edit)
+end
 # update
+post '/edit/:id' do
+    @student = Student.new(params)
+    @student.update()
+    redirect to '/'
+end
+
 
 # destroy
